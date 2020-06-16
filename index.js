8fe7882e61e7656965c3017f8db58335277b9887
andre petion <contact@oli-branch.com>
2020-06-16 11:50:16 -0400

    Msg

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 function stringIncludes(haystack, needle) {
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack.slice(i, needle.length) === needle) {
-      return true
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack.slice(i, needle.length) === needle) {
+            return true;
+        }
     }
-  }
 
-  return false
+    return false;
 }
 
 function countLetter(haystack, needle) {
-  let count = 0
-
-  while (haystack.length) {
-    if (haystack.shift() === needle) {
-      count++
+    let count = 0;
+    let i = 0;
+    while (i < haystack.length) {
+        if (haystack.substring(i, i + 1) === needle) {
+            count++;
+        }
+        i++;
     }
-  }
 
-  return count
+    return count;
 }
 
 function camelCase(sentence) {
-  const arr = sentence.split(' ')
+    const arr = sentence.split(" ");
 
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i].toLowerCase()
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].toLowerCase();
 
-    const letters = arr[i].split('')
-    letters[0] = letters[0].toUpperCase()
+        const letters = arr[i].split("");
+        letters[0] = letters[0].toUpperCase();
 
-    arr[i] = letters.join('')
-  }
+        arr[i] = letters.join("");
+    }
 
-  return arr.join('')
+    return arr.join("");
 }
 
 module.exports = {
-  stringIncludes,
-  countLetter,
-  camelCase,
-}
+    stringIncludes,
+    countLetter,
+    camelCase,
+};
